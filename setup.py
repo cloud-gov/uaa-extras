f4cd06bf1ed4c5fb3048794f29984bb8b529956c
Chris Nelson <cnelson@cnelson.org>
2016-05-08 20:58:09 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uaainvite',
+
+    version='0.0.1',
+
+    description='A simple UI for the UAA /invite_users endpoint',
+
+    url='https://github.com/18F/cg-uaa-invite',
+
+    author='Chris Nelson',
+    author_email='cnelson@cnelson.org',
+
+    license='Public Domain',
+
+    packages=find_packages(),
+
+    install_requires=[
+        'gunicorn',
+        'flask',
+        'requests',
+        'email_validator'
+    ],
+
+    test_suite='uaainvite.tests',
+
+    tests_require=[
+        'blinker',
+        'httmock',
+        'mock'
+    ]
+)
---
+++ uaainvite/__init__.py
@@ -0,0 +1,3 @@
+from uaainvite.webapp import create_app
+
+app = create_app()
