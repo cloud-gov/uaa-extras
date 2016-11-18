0636e9813af8a8d48afbe79b306959bac63db405
M. Adam Kendall <michael.kendall@gsa.gov>
2016-11-18 15:53:26 -0500

    Handle name change of this app

--- debug.py
+++ debug.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Run the webapp in debug mode"""
 
-from uaainvite.webapp import create_app
+from uaaextras.webapp import create_app
 
 if __name__ == "__main__":
     app = create_app()
