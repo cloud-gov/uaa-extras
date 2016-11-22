6fc941ec70c0a14accf23debcb21e4f70ef2950d
M. Adam Kendall <michael.kendall@gsa.gov>
2016-11-22 10:14:35 -0500

    Add in /change-password functionality, update invitations to /invite

--- debug.py
+++ debug.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Run the webapp in debug mode"""
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), ".tox/flake8/lib/python3.5/site-packages"))
 
 from uaaextras.webapp import create_app
 
