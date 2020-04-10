9b72f28b463058a29df82fe05a691c105802175c
Ben Berry <ben.berry@gsa.gov>
2020-04-10 15:48:47 -0700

    allow testing with user with unset totp_seed

--- integration_tests/integration_test.py
+++ integration_tests/integration_test.py
@@ -1,3 +1,5 @@
+import typing
+
 from bs4 import BeautifulSoup
 import pyotp
 import requests
@@ -19,8 +21,17 @@ class IntegrationTestClient:
         url = self.extras_url + page
         return self.s.post(url, **kwargs)
 
-    def log_in(self, username, password, totp_seed) -> None:
-        totp = pyotp.TOTP(totp_seed)
+    def log_in(self, username, password, totp_seed=None) -> typing.Tuple[str, bool]:
+        """
+        log in using the shibboleth totp IDP. 
+        handles registering totp if user does not have one currently
+        returns a tuple of str, bool representing the user's totp_seed, and whether a new totp was registered
+        """
+        if totp_seed is not None:
+            totp = pyotp.TOTP(totp_seed)
+        totp_updated = False
+        # query param used to track steps. Probably better to get it from responses, but /shrug/
+        s = 1
         r = self.s.get(self.uaa_url + "/login")
         params = {
             "returnIDParam": "idp",
@@ -44,29 +55,54 @@ class IntegrationTestClient:
             "shib_idp_ls_supported": "true",
             "_eventId_proceed": "",
         }
+        soup = BeautifulSoup(r.text, features="html.parser")
+        form = soup.find("form")
+        next_url = form.attrs['action']
+        print(form)
         r = self.s.post(
-            f"{self.idp_url}/profile/SAML2/POST/SSO",
-            params={"execution": "e1s1"},
+            f"{self.idp_url}{next_url}",
             data=payload,
         )
+        soup = BeautifulSoup(r.text, features="html.parser")
+        form = soup.find("form")
+        next_url = form.attrs['action']
+        print(form)
         payload = {
             "j_username": username,
             "j_password": password,
             "_eventId_proceed": "",
         }
         r = self.s.post(
-            f"{self.idp_url}/profile/SAML2/POST/SSO",
-            params={"execution": "e1s2"},
+            f"{self.idp_url}{next_url}",
             data=payload,
         )
+        soup = BeautifulSoup(r.text, features="html.parser")
+        form = soup.find("form")
+        print(form)
+        next_url = form.attrs['action']
+        if form is not None and "barcode" in str(form):
+            totp_updated = True
+            totp_seed = soup.find("strong").string
+            totp = pyotp.TOTP(totp_seed)
+            payload = {
+                "j_tokenNumber": totp.now(),
+                "_eventId_proceed": "",
+                "state": "$state",
+            }
+            r = self.s.post(
+                f"{self.idp_url}{next_url}",
+                data=payload,
+            )
+            soup = BeautifulSoup(r.text, features="html.parser")
+            form = soup.find("form")
+            next_url = form.attrs['action']
         payload = {
             "j_tokenNumber": totp.now(),
             "_eventId_proceed": "",
             "state": "$state",
         }
         r = self.s.post(
-            f"{self.idp_url}/profile/SAML2/POST/SSO",
-            params={"execution": "e1s3"},
+            f"{self.idp_url}{next_url}",
             data=payload,
         )
         soup = BeautifulSoup(r.text, features="html.parser")
@@ -76,6 +112,7 @@ class IntegrationTestClient:
         payload = dict(RelayState=relay_state, SAMLRequest=saml_request)
         r = self.s.post(action, data=payload)
         r = self.s.get(self.uaa_url)
+        return totp_seed, totp_updated
 
     def log_out(self) -> None:
         self.s.get(f"{self.uaa_url}/logout.do")
