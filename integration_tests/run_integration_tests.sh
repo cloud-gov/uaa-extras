bf57e33b93b5e0da6950f901e6b6b1a3bd7bc0e6
Ben Berry <ben.berry@gsa.gov>
2020-04-10 15:48:47 -0700

    allow testing with user with unset totp_seed

--- integration_tests/run_integration_tests.sh
+++ integration_tests/run_integration_tests.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-for required_var in EXTRAS_URL UAA_URL IDP_URL TEST_USERNAME TEST_PASSWORD TEST_TOKEN IDP_NAME; do
+# required environment vars:
+# EXTRAS_URL: the url for the uaaextras app, e.g. https://account.example.com
+# UAA_URL: the login url for uaa, e.g. https://login.example.com
+# IDP_URL: the url for the shibboleth IDP, e.g. https://idp.example.com
+# IDP_NAME: the name of the shibboleth IDP, as UAA sees it, e.g. example.com
+# TEST_USERNAME: the username of a user to run through tests
+# TEST_PASSWORD: the password for the user
+#
+# optional:
+# TEST_TOTP: the seed of the user's TOTP. Can be omitted if the user does not yet have a TOTP token set
+#
+
+for required_var in EXTRAS_URL UAA_URL IDP_URL TEST_USERNAME TEST_PASSWORD IDP_NAME; do
     if [[ -z ${!required_var} ]]; then
         echo "Please set ${required_var}"
         exit 1
     fi
 done
+
+if [[ -z ${TEST_TOTP} ]]; then
+    echo "TEST_TOTP is unset. Hope your user doesn't have a registered MFA!"
+fi
 
 pushd "$(dirname "${BASH_SOURCE[0]}")"
 
