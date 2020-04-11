050c62d9637f835b09de231479c08a446e07936e
Ben Berry <ben.berry@gsa.gov>
2020-04-10 17:22:44 -0700

    add test for login with no totp reuse csrf logic start work on invites test

--- integration_tests/test_uaa.py
+++ integration_tests/test_uaa.py
@@ -51,6 +51,12 @@ def authenticated(unauthenticated, user):
     return unauthenticated
 
 
+def get_csrf(page_text) -> str:
+    page = BeautifulSoup(page_text, features="html.parser")
+    csrf = page.find(attrs={"name": "_csrf_token"}).attrs["value"]
+    return csrf
+
+
 @pytest.mark.parametrize("page", ["/invite", "/change-password", "/first-login"])
 def test_unauthenticated_pages_redirect(unauthenticated, page, config):
     r = unauthenticated.get_page(page)
@@ -63,6 +69,15 @@ def test_authenticated_pages_work(authenticated, page, config):
     r = authenticated.get_page(page)
     assert r.status_code == 200
     assert r.url == config["urls"]["extras"] + page
+
+
+# NOTE: Needs to be first test as long as we do not have a totp-reset method
+def test_login_no_totp(unauthenticated, config, user):
+    token, changed = unauthenticated.log_in(user["name"], user["password"])
+    os.environ["TEST_TOKEN"] = token
+    assert changed
+    _, changed = unauthenticated.log_in(user["name"], user["password"], token)
+    assert not changed
 
 
 def test_change_password(authenticated, config, user):
@@ -80,11 +95,37 @@ def test_change_password(authenticated, config, user):
 
     # set the password back so we don't confuse the other tests
     r = authenticated.get_page("/change-password")
-    soup = BeautifulSoup(r.text, features="html.parser")
-    csrf = soup.find(attrs={"name": "_csrf_token"}).attrs["value"]
+    csrf = get_csrf(r.text)
     data = {
         "old_password": "a_severely_insecure_password",
         "new_password": user["password"],
         "repeat_password": user["password"],
         "_csrf_token": csrf,
     }
+
+
+@pytest.mark.skip("Not done yet")
+def test_invites_happy_path(authenticated, config):
+    if "dev" in config["urls"]["uaa"]:
+        # alternate path: use dev, but expect the request to fail
+        # email fails, but its also the last thing to happen, so the user
+        # is created and their invite info can still be fetched from Redis
+        pytest.skip("Can't test functions that require email in dev")
+    r = authenticated.get_page("/invite")
+    csrf = get_csrf(r.text)
+    soup = BeautifulSoup(r.text, features="html.parser")
+    form = soup.find("form")
+    url = form.attrs["action"]
+    payload = {"email": "", "_csrf_token": csrf}
+    r = authenticated.post_to_page(url, data=payload)
+    soup = BeautifulSoup(r.text, features="html.parser")
+    # TODO: finish this.
+    # Happy path sketch:
+    # - get the invite info straight from Redis
+    # - redeem invite
+    # - log in for the first time
+
+    # other tests to run:
+    # - bad email
+    # - bad csrf
+    # - no csrf
