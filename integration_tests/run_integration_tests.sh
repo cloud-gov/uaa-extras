b22ed3b26ab1619b82775fa2e01349856919477f
Ben Berry <ben.berry@gsa.gov>
2020-04-29 17:15:50 -0700

    create users in python tests, not bash wrappers

--- integration_tests/run_integration_tests.sh
+++ integration_tests/run_integration_tests.sh
@@ -5,36 +5,34 @@
 # UAA_URL: the login url for uaa, e.g. https://login.example.com
 # IDP_URL: the url for the shibboleth IDP, e.g. https://idp.example.com
 # IDP_NAME: the name of the shibboleth IDP, as UAA sees it, e.g. example.com
-# TEST_USERNAME: the username of a user to run through tests
-# TEST_PASSWORD: the password for the user
 #
 # optional:
-# TEST_TOTP: the seed of the user's TOTP. Can be omitted if the user does not yet have a TOTP token set
 #
 
-for required_var in EXTRAS_URL UAA_URL IDP_URL TEST_USERNAME TEST_PASSWORD IDP_NAME; do
+for required_var in EXTRAS_URL UAA_URL IDP_URL IDP_NAME UAA_USER UAA_SECRET; do
     if [[ -z ${!required_var} ]]; then
         echo "Please set ${required_var}"
         exit 1
     fi
 done
 
-if [[ -z ${TEST_TOTP} ]]; then
-    echo "TEST_TOTP is unset. Hope your user doesn't have a registered MFA!"
-fi
-
 pushd "$(dirname "${BASH_SOURCE[0]}")"
 
-python3 -m venv venv
+    if [[ -z "${NO_CREATE_ENV}" ]]; then
+        python3 -m venv venv
+        source venv/bin/activate
+        python3 -m pip install -r requirements.txt
 
-source venv/bin/activate
+        pushd ../
+            python3 setup.py install
+        popd
+    fi
+    pytest -v
+    return=$?
 
-python3 -m pip install -r requirements.txt
-
-pytest -v
-return=$?
-
-deactivate
+    if [[ -z "${NO_CREATE_ENV}" ]]; then
+        deactivate
+    fi
 
 popd
 exit ${return}
