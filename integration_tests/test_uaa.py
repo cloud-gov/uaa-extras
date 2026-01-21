5d0bfceae32c242070667718e40e7865beb8958b
David Anderson <david.n.anderson@gsa.gov>
2026-01-21 15:14:12 -0500

    test ouath login

--- integration_tests/test_uaa.py
+++ integration_tests/test_uaa.py
@@ -106,8 +106,8 @@ def test_login_no_totp(unauthenticated, config, user):
 
 
 def test_reset_totp(authenticated, user):
-    r = authenticated.get_page("/login")
     # get the page so we have a CSRF
+    r = authenticated.get_page("/oauth/login?code=" + user["token"])
     r = authenticated.get_page("/reset-totp")
     assert r.status_code == 200
 
