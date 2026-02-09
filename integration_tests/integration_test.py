571fc795ad3e659feb7b40f2059d294c14098776
Van Nguyen <vanh.nguyen@gsa.gov>
2026-02-09 15:06:08 -0500

    print tokjen

--- integration_tests/integration_test.py
+++ integration_tests/integration_test.py
@@ -118,6 +118,7 @@ class IntegrationTestClient:
         next_url = form.attrs["action"]
         csrf = get_csrf_for_form(form)
         r = self.idp_username_password_login(next_url, username, password, csrf)
+        print("CSRF before totp login: " + csrf)
         totp_seed, totp_updated, r = self.idp_totp_login(r.text, totp_seed)
         print("totp seed" + totp_seed)
         print("totp updated" + str(totp_updated))
