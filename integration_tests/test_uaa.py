313e3f6fb07fb3573c3f217a7b97b27b7847bdbe
Van Nguyen <vanh.nguyen@gsa.gov>
2026-02-09 10:59:51 -0500

    print resetotop

--- integration_tests/test_uaa.py
+++ integration_tests/test_uaa.py
@@ -108,7 +108,7 @@ def test_login_no_totp(unauthenticated, config, user):
 def test_reset_totp(authenticated, user):
     # get the page so we have a CSRF
     r = authenticated.get_page("/reset-totp")
-
+    print(r.text)
     csrf = get_csrf(r.text)
     # actually reset our totp
     r = authenticated.post_to_page("/reset-totp", data={"_csrf_token": csrf})
