9d19baaaf361c19169114047f0f4f30232736791
Van Nguyen <vanh.nguyen@gsa.gov>
2026-02-09 07:57:55 -0500

    pretotp

--- integration_tests/integration_test.py
+++ integration_tests/integration_test.py
@@ -114,9 +114,9 @@ class IntegrationTestClient:
         form = soup.find("form")
         next_url = form.attrs["action"]
         csrf = get_csrf_for_form(form)
-        print(r.text)
-        print("preloging: " + csrf)
         r = self.idp_username_password_login(next_url, username, password, csrf)
+        print(r.text)
+        print("pretotp: " + csrf)
         totp_seed, totp_updated, r = self.idp_totp_login(r.text, totp_seed)
 
         soup = BeautifulSoup(r.text, features="html.parser")
