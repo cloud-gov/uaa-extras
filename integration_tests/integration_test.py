aa18572202fb63a982fefdc0dd6db38fe315fff5
David Anderson <david.n.anderson@gsa.gov>
2026-02-10 15:29:48 -0500

    remove tests

--- integration_tests/integration_test.py
+++ integration_tests/integration_test.py
@@ -101,8 +101,6 @@ class IntegrationTestClient:
         print("final next url: " + next_url)
         r = self.s.post(f"{self.idp_url}{next_url}", data=payload)
         print(r.text)
-        r = self.s.post(f"{self.idp_url}{next_url}", data=payload)
-        print(r.text)
         return totp_seed, totp_updated, r
 
 
