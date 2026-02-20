5394bfe0fcd2fce0a79013fe82733867e48b363c
David Anderson <david.n.anderson@gsa.gov>
2026-02-20 17:34:00 -0500

    try formatting

--- integration_tests/integration_test.py
+++ integration_tests/integration_test.py
@@ -143,10 +143,9 @@ class IntegrationTestClient:
         print(action)
         if csrf is not None:
             payload["csrf_token"] = csrf
-        r = self.s.post(action, data=payload)
+        r = self.s.post(f"{action}", data=payload)
         print("POST" + r.text)
-        r = self.s.post(self.uaa_url, data=payload)
-        # r = self.s.get(self.uaa_url)
+        r = self.s.get(self.uaa_url)
         return totp_seed, totp_updated
 
     def log_out(self) -> None:
